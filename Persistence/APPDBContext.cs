c0d51b8fa1afabadf253b86a3114165442c5999c
Abijan udara <padhmath_im17052@stu.kln.ac.lk>
2022-03-17 21:21:48 +0530

    Add project files.

---
+++ Persistence/APPDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Department_Backend.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Department_Backend.Persistence
+{
+    public class APPDBContext:DbContext
+    {
+        public APPDBContext(DbContextOptions<APPDBContext> options)
+       : base(options)
+        {
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
